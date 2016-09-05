629f92727a7d714fb6ef507af967434a67d5db26
huangyafeng <huangyafeng@huangyafeng.didichuxing.com>
2016-09-06 00:16:52 +0800

    修改 pom 依赖

--- src/main/java/com/sleep/binlog/BinlogMigration.java
+++ src/main/java/com/sleep/binlog/BinlogMigration.java
@@ -1,11 +1,17 @@
 package com.sleep.binlog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sleep.binlog.net.BinlogClient;
 import com.sleep.binlog.util.ThreadUtil;
 
 public class BinlogMigration {
 	
+	private static final Logger logger = LoggerFactory.getLogger(BinlogMigration.class);
+	
 	public static void main(String[] args) {
+		logger.info("BinlogMigration start ...");
 		ThreadUtil.newThread(new BinlogClient("localhost", 3306), "BinlogClient");
 	}
 
