a0d710d46a7c017597e0f2baa82ee580da407594
huangyafeng <huangyafeng@10.0.30.228>
2016-09-23 17:46:06 +0800

    优化 Connector 代码

--- src/main/java/com/sleep/binlog/net/Connector.java
+++ src/main/java/com/sleep/binlog/net/Connector.java
@@ -8,7 +8,12 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Connector {
+
+	private static final Logger logger = LoggerFactory.getLogger(Connector.class);
 
 	private Connection conn;
 
@@ -25,41 +30,53 @@ public class Connector {
 		this.password = password;
 		this.hostname = hostname;
 		this.port = port;
-		conn = DriverManager.getConnection("jdbc:mysql://" + hostname + ":" + port + "/information_schema?user="
+		conn = createConn(username, password, hostname, port);
+	}
+
+	private Connection createConn(String username, String password, String hostname, int port) throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://" + hostname + ":" + port + "/information_schema?user="
 				+ username + "&password=" + password + "&useUnicode=true&characterEncoding=UTF8");
 	}
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException, InterruptedException {
-		new Connector("canal", "123456", "10.0.30.152", 3306).getTableColumns("dump_test", "user");
-	}
-
 	public List<String> getTableColumns(String schema, String table) {
-		Statement stmt = null;
-		ResultSet rs = null;
-		try {
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery("select column_name from columns where table_schema = \"" + schema
-					+ "\" and table_name = \"" + table + "\"");
-			List<String> result = new ArrayList<String>();
-			while (rs.next()) {
-				result.add(rs.getString(1));
-			}
-			return result;
-		} catch (Exception e) {
-			// TODO
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e1) {
-					// ignore
+		for (int i = 0; i < 3; i++) {
+			Statement stmt = null;
+			ResultSet rs = null;
+			try {
+				if (i > 0) {
+					conn = createConn(username, password, hostname, port);
 				}
-			}
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e1) {
-					// ignore
+				stmt = conn.createStatement();
+				rs = stmt.executeQuery("select column_name from columns where table_schema = \"" + schema
+						+ "\" and table_name = \"" + table + "\"");
+				List<String> result = new ArrayList<String>();
+				while (rs.next()) {
+					result.add(rs.getString(1));
+				}
+				return result;
+			} catch (Exception e) {
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException e1) {
+						// ignore
+					}
+				}
+				logger.error("Get table columns failed.", e);
+			} finally {
+				if (rs != null) {
+					try {
+						rs.close();
+					} catch (SQLException e1) {
+						// ignore
+					}
+				}
+				if (stmt != null) {
+					try {
+						stmt.close();
+					} catch (SQLException e1) {
+						// ignore
+					}
 				}
 			}
 		}
